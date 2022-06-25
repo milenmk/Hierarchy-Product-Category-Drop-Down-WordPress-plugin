a8f8551339c3d63c1a371768ee7bcf187019f9d3
Milen Karaganski <milen@blacktiehost.com>
2022-06-25 19:49:59 +0300

    minor

--- hpcdd.php
+++ hpcdd.php
@@ -14,7 +14,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Hierarchy Product Category Drop Down
- * Plugin URI:        https://blacktiehost.com
+ * Plugin URI:        https://wordpress.org/plugins/hpcdd/
  * Description:       Show hierarchy dropdown to search woocommerce products per category.
  * Version:           1.0.0
  * Author:            Milen Karaganski
